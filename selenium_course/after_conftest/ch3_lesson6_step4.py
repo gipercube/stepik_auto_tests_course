23383e2bdbdfab6cdb3b5a1fd175f4978dedb025
gipercube <a.n.kozelskikh@gmail.com>
2022-09-05 21:19:37 +0300

    Новые уроки

--- selenium_course/after_conftest/ch3_lesson6_step4.py
+++ selenium_course/after_conftest/ch3_lesson6_step4.py
@@ -13,17 +13,19 @@ def browser():
     browser.implicitly_wait(15)
     yield browser
     print("\nquit browser..")
+    browser.quit()
 
 
-links = ['http://stepik.org/lesson/236895/step/1']
-#,'https://stepik.org/lesson/236896/step/1','https://stepik.org/lesson/236897/step/1','https://stepik.org/lesson/236898/step/1','https://stepik.org/lesson/236899/step/1','https://stepik.org/lesson/236903/step/1','https://stepik.org/lesson/236904/step/1','https://stepik.org/lesson/236905/step/1']
+links = ['https://stepik.org/lesson/236895/step/1','https://stepik.org/lesson/236896/step/1','https://stepik.org/lesson/236897/step/1','https://stepik.org/lesson/236898/step/1','https://stepik.org/lesson/236899/step/1','https://stepik.org/lesson/236903/step/1','https://stepik.org/lesson/236904/step/1','https://stepik.org/lesson/236905/step/1']
 
-answer = math.log(int(time.time()))
 
 @pytest.mark.parametrize('links', links)
 def test_guest_should_see_login_link(browser, links):
     link = f"{links}"
     browser.get(link)
-    textarea = browser.find_element(By.CSS_SELECTOR, "#ember87")
+    textarea = browser.find_element(By.CSS_SELECTOR, "textarea")
+    answer = math.log(int(time.time()))
     textarea.send_keys(answer)
     browser.find_element(By.CSS_SELECTOR, ".submit-submission").click()
+    verdict = browser.find_element(By.CSS_SELECTOR, ".smart-hints__hint").text
+    assert verdict == "Correct!", f"ВМЕСТО коррект отображается: {verdict}"
